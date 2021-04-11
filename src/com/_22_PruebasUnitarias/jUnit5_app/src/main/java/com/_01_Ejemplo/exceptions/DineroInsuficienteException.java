719cc715940dcb893744c88788d79670b0454bf8
danielfernandoyepezvelez <danielfernandoyepezvelez>
2021-04-10 19:52:42 -0500

    Eliminando Carpeta 22

--- src/com/_22_PruebasUnitarias/jUnit5_app/src/main/java/com/_01_Ejemplo/exceptions/DineroInsuficienteException.java
+++
@@ -1,8 +0,0 @@
-package com._01_Ejemplo.exceptions;
-
-public class DineroInsuficienteException extends RuntimeException {
-
-    public DineroInsuficienteException(String message) {
-        super(message);
-    }
-}
