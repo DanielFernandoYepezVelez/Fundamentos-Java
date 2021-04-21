32a36e2edc970b5834a43250637883eb7b2e4900
danielfernandoyepezvelez <danielfernandoyepezvelez>
2021-04-20 20:57:47 -0500

    Mejorando Operadores

--- src/com/_04_Operadores/_03_Unarios.java
+++ src/com/_04_Operadores/_03_Unarios.java
@@ -3,8 +3,10 @@ package com._04_Operadores;
 public class _03_Unarios {
     public static void OperadoresUnarios() {
         int i = -10, j = 13;
+        var verdadero = true;
+        var cambioVerdadero = !verdadero;
         
-        System.out.println("\n========== OPERADORES UNARIOS (Leyes De Los Signos) ==========");
+        System.out.println("\n========== OPERADORES UNARIOS (Leyes De Los Signos Y Booleanos) ==========");
         System.out.println("i = " + i);
         System.out.println("+i = " + (+i)); // => (1) * i;
         System.out.println("(-i) = " + (-i)); // => (-1) * i;
@@ -12,5 +14,8 @@ public class _03_Unarios {
         System.out.println("j = " + j);
         System.out.println("(+j) = " + (+j)); // => (1) * j;
         System.out.println("(-j) = " + (-j)); // => (-1) * j;
+
+        System.out.println("verdadero = " + verdadero);
+        System.out.println("Cambio Verdadero = " + cambioVerdadero);
     }
 }
